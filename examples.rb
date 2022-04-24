4c3dd94d064cb3c42c00f740fab3e9bce5fe615b
J05HL4CK <joshualack994@gmail.com>
2022-04-24 16:05:56 +1000

    examples files created for workbook, arrays added - show selecting elements via indices

---
+++ examples.rb
@@ -0,0 +1,11 @@
+# array example workbook
+
+friends = %W[Mary Bill Jane Tom Lizzy Matt]
+
+not_friends = ["Jim", "Jodie", "Miles", "Maddy"]
+
+everyone_else = %W[Fred Mark Tim Gabe Jon Bec Mandy Lisa ]
+
+people = [friends, not_friends, everyone_else]
+
+p people[0][0]
