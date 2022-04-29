58294627a0c53602ba3a937ec806512f13addc3d
J05HL4CK <joshualack994@gmail.com>
2022-04-29 21:57:24 +1000

    ruby data types examples for workbook resub

--- data.rb
+++ data.rb
@@ -1,2 +1,14 @@
 # Data types in Ruby, some examples;
 
+# The variable "textual" is assigned the class string 
+textual = "I am a string of characters representing textual data"
+# the variable "integer" is assigned the class integer
+integer = 1
+# "float" is a floating point number
+float = 5.0
+# "array" is an array containing numbers of different types
+array = [integer, float]
+# "hash" is a hash conting string: as the symbol and textual as the value
+hash = {string: textual}
+# Boolean true/false
+array.include?(integer) 
