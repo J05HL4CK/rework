38cb12db92a2bd426050969848c0335e0ace65a3
J05HL4CK <joshualack994@gmail.com>
2022-05-01 01:01:06 +1000

    ex 15 roughing out

---
+++ 15.rb
@@ -0,0 +1,16 @@
+temperature = gets.chomp.to_i
+raining = true
+case temperature
+when (0..15)
+  if raining == true
+    print 'It\'s wet and cold'
+  else
+    print 'It\'s not raining but is cold'
+  end
+when temperature >= 15
+  if raining == false
+    print 'It\'s warm and not raining'
+  else
+    print 'It\'s warm and raining'
+  end
+end
