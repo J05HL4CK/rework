cb71e251d4c549ca30e25ed24da2077e476b1f0b
J05HL4CK <joshualack994@gmail.com>
2022-04-30 14:18:46 +1000

    bubble sort answer for ex13 workbook resub

--- 13.rb
+++ 13.rb
@@ -1,9 +1,11 @@
 # look for the first two elements that are out of order and swap them
 
-
-array = [5, 22, 29, 39, 19, 51, 78, 96, 84]
-
-# i = array.each_cons(2).find_index { |a, b| a > b  }
-# array[i, 2] = array[i, 2].reverse if i
+# solution one - use multiple assignment instead of assigning both elements to arr[i + 1]
+arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+i = 0
+while i < arr.size and arr[i] < arr[i + 1]
+  i = i + 1 end
+puts i
+arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
 
---
+++ 13_2.rb
@@ -0,0 +1,18 @@
+arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+# finds largest numbers, swaps first two out of order,
+# disregarding the last elements out of order
+def bubbles(arr)
+  n = arr.size
+  (n - 2).times do |x| 
+    is_swapped = false
+    (n - x - 2).times do |index|
+      if arr[index] > arr[index + 1]
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        is_swapped = true
+      end
+    end
+    break unless is_swapped
+  end
+  arr
+end
+p bubbles(arr)
