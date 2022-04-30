45973d9fd1fed3441b8a4af0e604912a2c634af0
J05HL4CK <joshualack994@gmail.com>
2022-05-01 00:22:44 +1000

    refactored prime sieve

--- 14_2.rb
+++ 14_2.rb
@@ -1,24 +1,16 @@
+# frozen_string_literal: true
+
 multiples = []
 (1..100).each do |x|
-  if (x % 2).zero? && (x / 2) != 1
-    multiples << x
-  end
-  if (x % 3).zero? && (x / 3) != 1
-    multiples << x
-  end
-  if (x % 5).zero? && (x / 5) != 1
-    multiples << x
-  end
-  if (x % 7).zero? && (x / 7) != 1
-    multiples << x
-  end
-  if (x % 11).zero? && (x / 11) != 1
-    multiples << x
-  end
+  multiples << x if (x % 2).zero? && (x / 2) != 1
+  multiples << x if (x % 3).zero? && (x / 3) != 1
+  multiples << x if (x % 5).zero? && (x / 5) != 1
+  multiples << x if (x % 7).zero? && (x / 7) != 1
+  multiples << x if (x % 11).zero? && (x / 11) != 1
 end
 
 integers = []
-(2..100).each do |i| 
+(2..100).each do |i|
   integers << i
 end
 
